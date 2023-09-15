c8b67b248f07b84832fcd6205a800766e26c3636
Riley <woodruff.l.riley@gmail.com>
2023-09-15 16:54:11 -0700

    actually trying to make it work

--- caesar_gui.py
+++ caesar_gui.py
@@ -1,7 +1,5 @@
 import tkinter as tk
 import pygame
-import random
-import time
 
 
 def play_sound():
@@ -10,45 +8,27 @@ def play_sound():
     pygame.mixer.music.play()
 
 
-def create_confetti(canvas):
-    colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-    size = random.randint(10, 30)
-    x = random.randint(0, canvas.winfo_width())
-    y = 0
-    confetti = canvas.create_rectangle(
-        x, y, x + size, y + size, fill=random.choice(colors), tags="confetti"
-    )
-    return confetti
-
-
-def animate_confetti(canvas):
-    confetti_list = canvas.find_withtag("confetti")
-    for c in confetti_list:
-        """time.sleep(0.05)"""
-        canvas.move(c, 0, 2)
-    canvas.after(5, animate_confetti, canvas)
-
-
-def start_confetti():
-    animate_confetti(canvas)
+def encode(string, shift_no):
+    list = []
+    for letter in string:
+        list.append(chr(ord(letter) + shift_no))
+    return string(list)
 
 
 def on_button_click():
     play_sound()
-    for i in range(random.randint(40, 75)):
-        create_confetti(canvas)
-    start_confetti()
+    to_encode = encoded_text.get()
 
 
 root = tk.Tk()
 root.title("Caesar Cypher App")
+root.geometry("400x600")
 
+encoded_text = tk.Entry(root, text="text to encode", height=5, width=30)
+shift_amount = tk.Entry(root, text="Amount to shift given text by", height=4, width=4)
+encode_btn = tk.Button(root, text="encode", command=on_button_click)
 
-canvas = tk.Canvas(root, width=400, height=600, bg="white")
-canvas.pack()
-
-start_button = tk.Button(canvas, text="Start Confetti", command=on_button_click)
-start_button_window = canvas.create_window(10, 10, anchor=tk.NW, window=start_button)
+output = tk.Text(root, height=5, width=30)
 
 
 root.mainloop()
